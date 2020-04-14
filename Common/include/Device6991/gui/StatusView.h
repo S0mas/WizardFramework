fcf8e93caaccafb1f6d1b21b55433b9a1d070726
S0mas <sommerfeld.krzysztof@gmail.com>
2020-04-14 13:07:55 +0200

    removed not used code parts

--- Common/include/Device6991/gui/StatusView.h
+++ Common/include/Device6991/gui/StatusView.h
@@ -8,8 +8,6 @@ class StatusView : public QWidget {
 	Q_OBJECT
 	QLabel* masterIdLabel_ = new QLabel("Master Id:");
 	QLabel* masterIdValue_ = new QLabel;
-	QLabel* modeLabel_ = new QLabel("ControlMode:");
-	QLabel* modeValue_ = new QLabel;
 	QLabel* dataStreamLabel_ = new QLabel("Data Stream:");
 	QLabel* dataStreamValue_ = new QLabel;
 	QLabel* stateLabel_ = new QLabel("State:");
@@ -18,7 +16,6 @@ class StatusView : public QWidget {
 	QLabel* currentTimeValue_ = new QLabel;
 public:
 	StatusView(QWidget* parent = nullptr);
-	void setMode(QString const& mode) noexcept;
 public slots:
 	void update(DeviceState const& status) noexcept;
 };
